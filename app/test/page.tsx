8637a0c3d360dfe3ac87681406b2f5b45c31fe4d
Jake <mubok0212@gmail.com>
2025-06-18 18:10:04 +0900

    get partcipation page directly from server

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,9 +1,10 @@
-import PolisConversation from "@/app/components/PolisConversation";
+import PolisClient from "@/app/components/PolisClient";
 
 export default function TestPage() {
+ 
   return (
     <div className="flex justify-center w-[60%] mx-auto border border-gray-100/30 rounded-lg p-4 mt-24">
-      Hello
+      <PolisClient conversationId='93hsh7vtae' />
     </div>
   );
-}
+} 
